102abbafcd97e0a9912344b15f6a216d284ecbf1
alexvoneida <alexvoneida@gmail.com>
2025-09-30 12:10:40 -0600

    Remove testing files

--- practice.py
+++
@@ -1,7 +0,0 @@
-from nba_api.stats.static import teams
-from nba_api.stats.static import players
-import pandas as pd
-
-df = pd.read_parquet('player_game_logs_2024-25_TEST.parquet')
-
-print(df.head())
